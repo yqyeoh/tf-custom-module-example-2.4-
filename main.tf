490424f8578bf96bf90565f043839068f64449a4
yqyeoh <yqyeoh@hotmail.com>
2026-07-22 20:50:14 +0800

    tf custom module

---
+++ main.tf
@@ -0,0 +1,5 @@
+module "app_topics" {
+  source      = "./modules/app-topics"
+  name_prefix = "yq"
+  cart_count  = 2
+}
---
+++ modules/app-topics/variables.tf
@@ -0,0 +1,8 @@
+variable "name_prefix" {
+  type = string
+}
+
+variable "cart_count" {
+  type    = number
+  default = 0
+}
